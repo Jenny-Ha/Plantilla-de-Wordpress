22c319391dea02969419f6b5cf60b4ac88c9fb4d
Jenny <jennyha069@gmail.com>
2016-10-27 19:35:46 -0500

    Creación de template especial para una página de WP

---
+++ template-fullwidth.php
@@ -0,0 +1,28 @@
+<?php 
+/*
+Template name: Fullwidth
+*/
+?>
+<?php get_header(); ?>
+		
+		<section id="global-content" class="cf">
+			
+			<section id="main-content" class="fullwidth">
+				
+			<?php if( have_posts() ) : while (have_posts() ) : the_post(); ?>
+				<article class="post">
+					<header class="post-title">
+						<h1><?php the_title(); ?></h1>
+					</header>
+					<div class="post-content">
+						<?php the_content(); ?>
+					</div>
+				</article>	<!-- article -->
+			<?php endwhile; endif; ?>
+				
+
+			</section><!-- /#main-content -->
+			
+		</section><!-- /#global-content -->
+		
+		<?php get_footer(); ?>
